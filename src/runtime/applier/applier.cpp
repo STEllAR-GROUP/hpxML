STEllAR-GROUP/hpxML

commit 41990320cc5530f6d50b036c5044116067273d1e

parcel routing implementation.
git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@6164 5079c8e4-9419-0410-93bb-9e5ee278c886

src/runtime/applier/applier.cpp
@@ -304,6 +304,7 @@ namespace hpx { namespace applier
                     "applier::address_is_local",
                     hpx::util::osstream_get_string(strm));
             }
+            hpx::applier::get_applier().get_agas_client().resolve(id, addr, true, throws);
             return true;
         }
 
@@ -319,6 +320,40 @@ namespace hpx { namespace applier
             strm << "gid" << id.get_gid();
             HPX_THROW_EXCEPTION(invalid_status,
                 "applier::address_is_local",
+                hpx::util::osstream_get_string(strm));
+        }
+        return false;   // non-local
+    }
+
+    bool applier::address_is_local_c_cache(naming::id_type const& id, 
+        naming::address& addr) const
+    {
+        bool is_local = id.is_local_c_cache();
+        bool in_cache = id.get_local_address_c_cache(addr);
+        if(is_local) {     // check address if it is in cache
+            if (!in_cache) {
+                hpx::util::osstream strm;
+                strm << "gid" << id.get_gid();
+                HPX_THROW_EXCEPTION(invalid_status, 
+                    "applier::address_is_local_c_cache", 
+                    hpx::util::osstream_get_string(strm));
+            }
+            hpx::applier::get_applier().get_agas_client().resolve_cached(id.get_gid(), addr, throws);
+            return true;
+        }
+
+        if (!id.is_resolved() && is_local) {
+            hpx::util::osstream strm;
+            strm << "gid" << id.get_gid();
+            HPX_THROW_EXCEPTION(unknown_component_address, 
+                "applier::address_is_local_c_cache", hpx::util::osstream_get_string(strm));
+        }
+
+        if (!id.get_address_cached(addr) && in_cache) {
+            hpx::util::osstream strm;
+            strm << "gid" << id.get_gid();
+            HPX_THROW_EXCEPTION(invalid_status, 
+                "applier::address_is_local_c_cache", 
                 hpx::util::osstream_get_string(strm));
         }
         return false;   // non-local
@@ -350,6 +385,51 @@ namespace hpx { namespace applier
                 "applier::address_is_local", hpx::util::osstream_get_string(strm));
         }
         return addr.locality_ == parcel_handler_.here();
+    }
+
+    bool applier::address_is_local_c_cache(naming::gid_type const& gid, 
+        naming::address& addr) const
+    {
+        // test if the gid is of one of the non-movable objects
+        // this is certainly an optimization relying on the fact that the 
+        // lsb of the local objects is equal to their address
+        if (naming::strip_credit_from_gid(gid.get_msb()) == 
+                parcel_handler_.get_prefix().get_msb())
+        {
+            // a zero address references the local runtime support component
+            if (0 != gid.get_lsb())
+                addr.address_ = gid.get_lsb();
+            else 
+                addr.address_ = runtime_support_id_.get_lsb();
+            return true;
+        }
+
+        // Resolve the address of the gid
+        if (!parcel_handler_.get_resolver().resolve_cached(gid, addr))
+        {
+            hpx::util::osstream strm;
+            strm << "gid" << gid;
+            HPX_THROW_EXCEPTION(unknown_component_address, 
+                "applier::address_is_local", hpx::util::osstream_get_string(strm));
+        }
+        return addr.locality_ == parcel_handler_.here();
+    }
+
+    // parcel forwarding.
+    bool applier::route(parcelset::parcel const& p)
+    {
+        //hpx::applier::applier& appl = hpx::applier::get_applier();
+        get_agas_client().route_parcel(p);
+        return true;
+    }
+
+    /*bool applier::check_if_bootstrap()
+    {
+        return get_agas_client().is_bootstrap();
+    }*/
+    bool applier::test_bootstrap()
+    {
+        return get_agas_client().is_bootstrap();
     }
 
     void applier::init_tss()

examples/accumulator/accumulator/stubs/accumulator.hpp
@@ -51,21 +51,21 @@ namespace hpx { namespace components { namespace stubs
         /// with the given \a gid
         static void init(naming::id_type gid)
         {
-            applier::apply<server::accumulator::init_action>(gid);
+            applier::apply_route<server::accumulator::init_action>(gid);
         }
 
         /// Add the given number to the server#accumulator instance
         /// with the given \a gid
         static void add (naming::id_type gid, unsigned long arg)
         {
-            applier::apply<server::accumulator::add_action>(gid, arg);
+            applier::apply_route<server::accumulator::add_action>(gid, arg);
         }
 
         /// Print the current value of the server#accumulator instance
         /// with the given \a gid
         static void print(naming::id_type gid)
         {
-            applier::apply<server::accumulator::print_action>(gid);
+            applier::apply_route<server::accumulator::print_action>(gid);
         }
     };
 

hpx/runtime/agas/addressing_service.hpp
@@ -1092,6 +1092,13 @@ public:
       , gva const& gva
       , error_code& ec = throws
         );
+
+    
+    bool route_parcel (
+        parcelset::parcel const& arg0
+        );
+    
+
 };
 
 }}

hpx/runtime/agas/primary_namespace.hpp
@@ -37,6 +37,22 @@ struct primary_namespace :
     {
         return this->base_type::service(this->gid_, req, ec);
     }
+
+    lcos::promise<bool> route_async(
+        parcelset::parcel const& p
+        )
+    {
+        return this->base_type::route_async(this->gid_, p);
+    }
+
+    bool route(
+        parcelset::parcel const& p
+      , error_code& ec = throws
+      )
+    {
+        return this->base_type::route(this->gid_, p, ec);
+    }
+ 
 };
 
 }}

hpx/runtime/agas/server/primary_namespace.hpp
@@ -126,6 +126,18 @@ struct HPX_EXPORT primary_namespace :
       , prefix_counter_(0)
     {}
 
+    bool route(
+        parcelset::parcel const& p
+        )
+    {
+        return route(p, throws);
+    }
+
+    bool route(
+        parcelset::parcel const& p
+      , error_code& ec
+      );
+
     response service(
         request const& req
         )
@@ -198,6 +210,7 @@ struct HPX_EXPORT primary_namespace :
       , namespace_increment        = BOOST_BINARY_U(1000111)
       , namespace_decrement        = BOOST_BINARY_U(1001000)
       , namespace_localities       = BOOST_BINARY_U(1001001)
+      , namespace_route            = BOOST_BINARY_U(1001011)
     }; // }}}
 
     typedef hpx::actions::result_action1<
@@ -208,6 +221,15 @@ struct HPX_EXPORT primary_namespace :
       , &primary_namespace::service
       , threads::thread_priority_critical
     > service_action;
+
+    typedef hpx::actions::result_action1<
+        primary_namespace
+      , /* return type */ bool
+      , /* enum value */ namespace_route
+      , /* arguments */ parcelset::parcel const&
+      , &primary_namespace::route
+      , threads::thread_priority_critical
+    > route_action;
 };
 
 }}}

hpx/runtime/agas/server/symbol_namespace.hpp
@@ -99,6 +99,7 @@ struct HPX_EXPORT symbol_namespace :
       , namespace_resolve = BOOST_BINARY_U(0010010)
       , namespace_unbind  = BOOST_BINARY_U(0010011)
       , namespace_iterate = BOOST_BINARY_U(0010100)
+      , namespace_route   = BOOST_BINARY_U(0010101)
     }; // }}}
 
     typedef hpx::actions::result_action1<
@@ -109,6 +110,7 @@ struct HPX_EXPORT symbol_namespace :
       , &symbol_namespace::service
       , threads::thread_priority_critical
     > service_action;
+
 };
 
 }}}

hpx/runtime/agas/stubs/primary_namespace.hpp
@@ -39,6 +39,25 @@ struct primary_namespace
     {
         return service_async<response>(gid, req).get(ec);
     }
+
+    static lcos::promise<bool> route_async(
+        naming::id_type const& gid
+      , parcelset::parcel const& p
+      )
+    {
+        typedef server_type::route_action action_type;
+        return lcos::eager_future<action_type, bool>(gid, p);
+    }
+
+    static bool route(
+        naming::id_type const& gid
+      , parcelset::parcel const& p
+      , error_code& ec = throws
+      )
+    {
+        return route_async(gid, p).get(ec);
+    }
+
 };
 
 }}}

hpx/runtime/applier/applier.hpp
@@ -158,9 +158,23 @@ namespace hpx { namespace applier
         /// Test whether the given address (gid) is local or remote
         bool address_is_local(naming::id_type const& id,
             naming::address& addr) const;
+        
+        //check cache
+        bool address_is_local_c_cache(naming::id_type const& id,
+            naming::address& addr) const;
 
         bool address_is_local(naming::gid_type const& gid,
             naming::address& addr) const;
+
+        //check cache
+        bool address_is_local_c_cache(naming::gid_type const& gid,
+            naming::address& addr) const;
+
+        //parcel forwarding
+        bool 
+        route (parcelset::parcel const& arg0);
+
+        bool test_bootstrap();
 
     public:
         // the TSS holds a pointer to the applier associated with a given

commit b28d56b0b5a8033aa97a9c172a1f945e46a741c6

Applier: Revert bug introduced in r6164.
git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/trunk/hpx@6195 5079c8e4-9419-0410-93bb-9e5ee278c886

src/runtime/applier/applier.cpp
@@ -304,7 +304,6 @@ namespace hpx { namespace applier
                     "applier::address_is_local",
                     hpx::util::osstream_get_string(strm));
             }
-            hpx::applier::get_applier().get_agas_client().resolve(id, addr, true, throws);
             return true;
         }
 