dailypips/hpx

Added C++ standard library feature tests.

cmake/tests/cxx11_std_is_null_pointer.cpp
@@ -1,22 +1,14 @@
 ////////////////////////////////////////////////////////////////////////////////
-//  Copyright (c) 2011 Bryce Adelstein-Lelbach
+//  Copyright (c) 2015 Agustin Berge
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ////////////////////////////////////////////////////////////////////////////////
 
-#include <initializer_list>
-#include <vector>
- 
-template <typename T>
-struct A
-{
-    std::vector<T> v_;
+#include <cstddef>
+#include <type_traits>
 
-    A(std::initializer_list<T> v) : v_(v) {}
-};
- 
 int main()
 {
-    A<int> a = {1, 2, 3, 4, 5};
+    int check_nullptr[std::is_nullptr<std::nullptr_t>::value ? 1 : -1];
 }

STEllAR-GROUP/hpxML

cmake/tests/cxx11_std_is_null_pointer.cpp
@@ -1,22 +1,14 @@
 ////////////////////////////////////////////////////////////////////////////////
-//  Copyright (c) 2011 Bryce Adelstein-Lelbach
+//  Copyright (c) 2015 Agustin Berge
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ////////////////////////////////////////////////////////////////////////////////
 
-#include <initializer_list>
-#include <vector>
- 
-template <typename T>
-struct A
-{
-    std::vector<T> v_;
+#include <cstddef>
+#include <type_traits>
 
-    A(std::initializer_list<T> v) : v_(v) {}
-};
- 
 int main()
 {
-    A<int> a = {1, 2, 3, 4, 5};
+    int check_nullptr[std::is_null_pointer<std::nullptr_t>::value ? 1 : -1];
 }