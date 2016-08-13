Syntaf/hpx

Refactor vtables

hpx/util/detail/vtable/callable_vtable.hpp
@@ -1,6 +1,6 @@
 //  Copyright (c) 2011 Thomas Heller
 //  Copyright (c) 2013 Hartmut Kaiser
-//  Copyright (c) 2014 Agustin Berge
+//  Copyright (c) 2014-2015 Agustin Berge
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -8,11 +8,10 @@
 #ifndef HPX_UTIL_DETAIL_VTABLE_CALLABLE_VTABLE_HPP
 #define HPX_UTIL_DETAIL_VTABLE_CALLABLE_VTABLE_HPP
 
-#include <hpx/config/forceinline.hpp>
+#include <hpx/config.hpp>
 #include <hpx/util/detail/vtable/vtable.hpp>
 #include <hpx/util/invoke.hpp>
 
-#include <typeinfo>
 #include <utility>
 
 namespace hpx { namespace util { namespace detail
@@ -24,12 +23,17 @@ namespace hpx { namespace util { namespace detail
     struct callable_vtable<R(Ts...)>
     {
         template <typename T>
-        BOOST_FORCEINLINE static R invoke(void** f, Ts&&... vs)
+        BOOST_FORCEINLINE static R _invoke(void** f, Ts&&... vs)
         {
             return util::invoke<R>(
                 vtable::get<T>(f), std::forward<Ts>(vs)...);
         }
-        typedef R (*invoke_t)(void**, Ts&&...);
+        R (*invoke)(void**, Ts&&...);
+
+        template <typename T>
+        BOOST_CONSTEXPR callable_vtable(construct_vtable<T>) BOOST_NOEXCEPT
+          : invoke(&callable_vtable::template _invoke<T>)
+        {}
     };
 }}}
 

hpx/util/detail/vtable/function_vtable.hpp
@@ -1,0 +1,31 @@
+//  Copyright (c) 2011 Thomas Heller
+//  Copyright (c) 2013 Hartmut Kaiser
+//  Copyright (c) 2014-2015 Agustin Berge
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef HPX_UTIL_DETAIL_VTABLE_FUNCTION_VTABLE_HPP
+#define HPX_UTIL_DETAIL_VTABLE_FUNCTION_VTABLE_HPP
+
+#include <hpx/config.hpp>
+#include <hpx/util/detail/vtable/copyable_vtable.hpp>
+#include <hpx/util/detail/vtable/unique_function_vtable.hpp>
+#include <hpx/util/detail/vtable/vtable.hpp>
+
+namespace hpx { namespace util { namespace detail
+{
+    ///////////////////////////////////////////////////////////////////////
+    template <typename Sig>
+    struct function_vtable
+      : unique_function_vtable<Sig>, copyable_vtable
+    {
+        template <typename T>
+        BOOST_CONSTEXPR function_vtable(construct_vtable<T>) BOOST_NOEXCEPT
+          : unique_function_vtable<Sig>(construct_vtable<T>())
+          , copyable_vtable(construct_vtable<T>())
+        {}
+    };
+}}}
+
+#endif

hpx/util/detail/vtable/serializable_vtable.hpp
@@ -1,6 +1,6 @@
 //  Copyright (c) 2011 Thomas Heller
 //  Copyright (c) 2013 Hartmut Kaiser
-//  Copyright (c) 2014 Agustin Berge
+//  Copyright (c) 2014-2015 Agustin Berge
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -8,7 +8,7 @@
 #ifndef HPX_UTIL_DETAIL_VTABLE_SERIALIZABLE_VTABLE_HPP
 #define HPX_UTIL_DETAIL_VTABLE_SERIALIZABLE_VTABLE_HPP
 
-#include <hpx/config/forceinline.hpp>
+#include <hpx/config.hpp>
 #include <hpx/runtime/serialization/serialization_fwd.hpp>
 #include <hpx/util/detail/vtable/vtable.hpp>
 
@@ -17,23 +17,27 @@ namespace hpx { namespace util { namespace detail
     struct serializable_vtable
     {
         template <typename T>
-        static void save_object(void* const* v,
+        static void _save_object(void* const* v,
             serialization::output_archive& ar, unsigned version)
         {
             ar << vtable::get<T>(v);
         }
-        typedef void (*save_object_t)(void* const*,
-            serialization::output_archive&, unsigned);
+        void (*save_object)(void* const*, serialization::output_archive&, unsigned);
 
         template <typename T>
-        static void load_object(void** v,
+        static void _load_object(void** v,
             serialization::input_archive& ar, unsigned version)
         {
             vtable::default_construct<T>(v);
             ar >> vtable::get<T>(v);
         }
-        typedef void (*load_object_t)(void**,
-            serialization::input_archive&, unsigned);
+        void (*load_object)(void**, serialization::input_archive&, unsigned);
+
+        template <typename T>
+        BOOST_CONSTEXPR serializable_vtable(construct_vtable<T>) BOOST_NOEXCEPT
+          : save_object(&serializable_vtable::template _save_object<T>)
+          , load_object(&serializable_vtable::template _load_object<T>)
+        {}
     };
 }}}
 

STEllAR-GROUP/hpxML

hpx/util/detail/vtable/callable_vtable.hpp
@@ -1,6 +1,6 @@
 //  Copyright (c) 2011 Thomas Heller
 //  Copyright (c) 2013 Hartmut Kaiser
-//  Copyright (c) 2014 Agustin Berge
+//  Copyright (c) 2014-2015 Agustin Berge
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -13,7 +13,6 @@
 #include <hpx/util/detail/vtable/vtable.hpp>
 #include <hpx/util/invoke.hpp>
 
-#include <typeinfo>
 #include <utility>
 
 namespace hpx { namespace util { namespace detail
@@ -35,12 +34,17 @@ namespace hpx { namespace util { namespace detail
     struct callable_vtable<R(Ts...)> : callable_vtable_base
     {
         template <typename T>
-        HPX_FORCEINLINE static R invoke(void** f, Ts&&... vs)
+        HPX_FORCEINLINE static R _invoke(void** f, Ts&&... vs)
         {
             return util::invoke<R>(
                 vtable::get<T>(f), std::forward<Ts>(vs)...);
         }
-        typedef R (*invoke_t)(void**, Ts&&...);
+        R (*invoke)(void**, Ts&&...);
+
+        template <typename T>
+        HPX_CONSTEXPR callable_vtable(construct_vtable<T>) HPX_NOEXCEPT
+          : invoke(&callable_vtable::template _invoke<T>)
+        {}
     };
 }}}
 

hpx/util/detail/vtable/function_vtable.hpp
@@ -1,0 +1,31 @@
+//  Copyright (c) 2011 Thomas Heller
+//  Copyright (c) 2013 Hartmut Kaiser
+//  Copyright (c) 2014-2015 Agustin Berge
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef HPX_UTIL_DETAIL_VTABLE_FUNCTION_VTABLE_HPP
+#define HPX_UTIL_DETAIL_VTABLE_FUNCTION_VTABLE_HPP
+
+#include <hpx/config.hpp>
+#include <hpx/util/detail/vtable/copyable_vtable.hpp>
+#include <hpx/util/detail/vtable/unique_function_vtable.hpp>
+#include <hpx/util/detail/vtable/vtable.hpp>
+
+namespace hpx { namespace util { namespace detail
+{
+    ///////////////////////////////////////////////////////////////////////
+    template <typename Sig>
+    struct function_vtable
+      : unique_function_vtable<Sig>, copyable_vtable
+    {
+        template <typename T>
+        HPX_CONSTEXPR function_vtable(construct_vtable<T>) HPX_NOEXCEPT
+          : unique_function_vtable<Sig>(construct_vtable<T>())
+          , copyable_vtable(construct_vtable<T>())
+        {}
+    };
+}}}
+
+#endif

hpx/util/detail/vtable/serializable_vtable.hpp
@@ -1,6 +1,6 @@
 //  Copyright (c) 2011 Thomas Heller
 //  Copyright (c) 2013 Hartmut Kaiser
-//  Copyright (c) 2014 Agustin Berge
+//  Copyright (c) 2014-2015 Agustin Berge
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -17,23 +17,27 @@ namespace hpx { namespace util { namespace detail
     struct serializable_vtable
     {
         template <typename T>
-        static void save_object(void* const* v,
+        static void _save_object(void* const* v,
             serialization::output_archive& ar, unsigned version)
         {
             ar << vtable::get<T>(v);
         }
-        typedef void (*save_object_t)(void* const*,
-            serialization::output_archive&, unsigned);
+        void (*save_object)(void* const*, serialization::output_archive&, unsigned);
 
         template <typename T>
-        static void load_object(void** v,
+        static void _load_object(void** v,
             serialization::input_archive& ar, unsigned version)
         {
             vtable::default_construct<T>(v);
             ar >> vtable::get<T>(v);
         }
-        typedef void (*load_object_t)(void**,
-            serialization::input_archive&, unsigned);
+        void (*load_object)(void**, serialization::input_archive&, unsigned);
+
+        template <typename T>
+        HPX_CONSTEXPR serializable_vtable(construct_vtable<T>) HPX_NOEXCEPT
+          : save_object(&serializable_vtable::template _save_object<T>)
+          , load_object(&serializable_vtable::template _load_object<T>)
+        {}
     };
 }}}
 