EFLql/hpx

Cleaning up *_colocated functions
 - Use the newly introduced `primary_namespace::colocate_action`
 - Fixed up regestration macros

hpx/lcos/detail/async_colocated.hpp
@@ -49,13 +49,8 @@ namespace hpx { namespace detail
 }}
 
 #define HPX_REGISTER_ASYNC_COLOCATED_DECLARATION(Action, Name)                \
-/**/
-#define HPX_REGISTER_ASYNC_COLOCATED(Action, Name)                            \
-/**/
-#if 0
-#define HPX_REGISTER_ASYNC_COLOCATED_DECLARATION(Action, Name)                \
     HPX_UTIL_REGISTER_UNIQUE_FUNCTION_DECLARATION(                            \
-        void (hpx::naming::id_type, hpx::agas::response)                      \
+        void (hpx::naming::id_type, hpx::naming::gid_type)                    \
       , (hpx::util::functional::detail::async_continuation_impl<              \
             hpx::util::detail::bound_action<                                  \
                 Action                                                        \
@@ -70,7 +65,7 @@ namespace hpx { namespace detail
 
 #define HPX_REGISTER_ASYNC_COLOCATED(Action, Name)                            \
     HPX_UTIL_REGISTER_UNIQUE_FUNCTION(                                        \
-        void (hpx::naming::id_type, hpx::agas::response)                      \
+        void (hpx::naming::id_type, hpx::naming::gid_type)                    \
       , (hpx::util::functional::detail::async_continuation_impl<              \
             hpx::util::detail::bound_action<                                  \
                 Action                                                        \
@@ -82,7 +77,6 @@ namespace hpx { namespace detail
       , Name                                                                  \
     );                                                                        \
 /**/
-#endif
 
 namespace hpx { namespace detail
 {
@@ -103,7 +97,7 @@ namespace hpx { namespace detail
         typedef
             typename hpx::traits::extract_action<Action>::remote_result_type
         remote_result_type;
-        typedef agas::server::primary_namespace::resolve_gid_action action_type;
+        typedef agas::server::primary_namespace::colocate_action action_type;
 
         using util::placeholders::_2;
         return detail::async_continue_r<action_type, remote_result_type>(
@@ -150,7 +144,7 @@ namespace hpx { namespace detail
         typedef
             typename hpx::traits::extract_action<Action>::remote_result_type
         remote_result_type;
-        typedef agas::server::primary_namespace::resolve_gid_action action_type;
+        typedef agas::server::primary_namespace::colocate_action action_type;
 
         using util::placeholders::_2;
         return detail::async_continue_r<action_type, remote_result_type>(

hpx/runtime/applier/register_apply_colocated.hpp
@@ -8,6 +8,7 @@
 
 #include <hpx/config.hpp>
 #include <hpx/runtime/naming/name.hpp>
+#include <hpx/runtime/naming/id_type.hpp>
 #include <hpx/util/functional/colocated_helpers.hpp>
 #include <hpx/util/bind.hpp>
 #include <hpx/util/tuple.hpp>
@@ -36,13 +37,8 @@ namespace hpx { namespace detail
 }}
 
 #define HPX_REGISTER_APPLY_COLOCATED_DECLARATION(Action, Name)                \
-    /**/
-#define HPX_REGISTER_APPLY_COLOCATED(action, name)                            \
-    /**/
-#if 0
-#define HPX_REGISTER_APPLY_COLOCATED_DECLARATION(Action, Name)                \
     HPX_UTIL_REGISTER_UNIQUE_FUNCTION_DECLARATION(                            \
-        void (hpx::naming::id_type, hpx::agas::response)                      \
+        void (hpx::naming::id_type, hpx::naming::gid_type)                    \
       , (hpx::util::functional::detail::apply_continuation_impl<              \
             hpx::util::detail::bound_action<                                  \
                 Action                                                        \
@@ -57,7 +53,7 @@ namespace hpx { namespace detail
 
 #define HPX_REGISTER_APPLY_COLOCATED(action, name)                            \
     HPX_UTIL_REGISTER_UNIQUE_FUNCTION(                                        \
-        void (hpx::naming::id_type, hpx::agas::response)                      \
+        void (hpx::naming::id_type, hpx::naming::gid_type)                    \
       , (hpx::util::functional::detail::apply_continuation_impl<              \
             hpx::util::detail::bound_action<                                  \
                 action                                                        \
@@ -69,6 +65,5 @@ namespace hpx { namespace detail
       , name                                                                  \
     );                                                                        \
 /**/
-#endif
 
 #endif

hpx/util/functional/colocated_helpers.hpp
@@ -16,7 +16,6 @@
 #include <hpx/throw_exception.hpp>
 #include <hpx/util/decay.hpp>
 #include <hpx/util/result_of.hpp>
-#include <hpx/util/tuple.hpp>
 
 #include <memory>
 #include <utility>
@@ -31,15 +30,10 @@ namespace hpx { namespace util { namespace functional
     {
         extract_locality() {}
 
-        typedef
-            hpx::util::tuple<naming::gid_type, agas::gva, naming::gid_type>
-            resolved_type;
-
-        naming::id_type operator()(resolved_type const& rep,
+        naming::id_type operator()(naming::gid_type const& locality_gid,
             naming::id_type const& id) const
         {
-            naming::id_type locality_id(
-                hpx::util::get<2>(rep), naming::id_type::unmanaged);
+            naming::id_type locality_id(locality_gid, naming::id_type::unmanaged);
             if(locality_id == naming::invalid_id)
             {
                 HPX_THROW_EXCEPTION(hpx::no_success,

STEllAR-GROUP/hpxML

hpx/lcos/detail/async_colocated.hpp
@@ -49,13 +49,8 @@ namespace hpx { namespace detail
 }}
 
 #define HPX_REGISTER_ASYNC_COLOCATED_DECLARATION(Action, Name)                \
-/**/
-#define HPX_REGISTER_ASYNC_COLOCATED(Action, Name)                            \
-/**/
-#if 0
-#define HPX_REGISTER_ASYNC_COLOCATED_DECLARATION(Action, Name)                \
     HPX_UTIL_REGISTER_UNIQUE_FUNCTION_DECLARATION(                            \
-        void (hpx::naming::id_type, hpx::agas::response)                      \
+        void (hpx::naming::id_type, hpx::naming::id_type)                     \
       , (hpx::util::functional::detail::async_continuation_impl<              \
             hpx::util::detail::bound_action<                                  \
                 Action                                                        \
@@ -70,7 +65,7 @@ namespace hpx { namespace detail
 
 #define HPX_REGISTER_ASYNC_COLOCATED(Action, Name)                            \
     HPX_UTIL_REGISTER_UNIQUE_FUNCTION(                                        \
-        void (hpx::naming::id_type, hpx::agas::response)                      \
+        void (hpx::naming::id_type, hpx::naming::id_type)                     \
       , (hpx::util::functional::detail::async_continuation_impl<              \
             hpx::util::detail::bound_action<                                  \
                 Action                                                        \
@@ -82,7 +77,6 @@ namespace hpx { namespace detail
       , Name                                                                  \
     );                                                                        \
 /**/
-#endif
 
 namespace hpx { namespace detail
 {
@@ -103,7 +97,7 @@ namespace hpx { namespace detail
         typedef
             typename hpx::traits::extract_action<Action>::remote_result_type
         remote_result_type;
-        typedef agas::server::primary_namespace::resolve_gid_action action_type;
+        typedef agas::server::primary_namespace::colocate_action action_type;
 
         using util::placeholders::_2;
         return detail::async_continue_r<action_type, remote_result_type>(
@@ -150,7 +144,7 @@ namespace hpx { namespace detail
         typedef
             typename hpx::traits::extract_action<Action>::remote_result_type
         remote_result_type;
-        typedef agas::server::primary_namespace::resolve_gid_action action_type;
+        typedef agas::server::primary_namespace::colocate_action action_type;
 
         using util::placeholders::_2;
         return detail::async_continue_r<action_type, remote_result_type>(

hpx/runtime/applier/register_apply_colocated.hpp
@@ -8,6 +8,7 @@
 
 #include <hpx/config.hpp>
 #include <hpx/runtime/naming/name.hpp>
+#include <hpx/runtime/naming/id_type.hpp>
 #include <hpx/util/functional/colocated_helpers.hpp>
 #include <hpx/util/bind.hpp>
 #include <hpx/util/tuple.hpp>
@@ -36,13 +37,8 @@ namespace hpx { namespace detail
 }}
 
 #define HPX_REGISTER_APPLY_COLOCATED_DECLARATION(Action, Name)                \
-    /**/
-#define HPX_REGISTER_APPLY_COLOCATED(action, name)                            \
-    /**/
-#if 0
-#define HPX_REGISTER_APPLY_COLOCATED_DECLARATION(Action, Name)                \
     HPX_UTIL_REGISTER_UNIQUE_FUNCTION_DECLARATION(                            \
-        void (hpx::naming::id_type, hpx::agas::response)                      \
+        void (hpx::naming::id_type, hpx::naming::id_type)                     \
       , (hpx::util::functional::detail::apply_continuation_impl<              \
             hpx::util::detail::bound_action<                                  \
                 Action                                                        \
@@ -57,7 +53,7 @@ namespace hpx { namespace detail
 
 #define HPX_REGISTER_APPLY_COLOCATED(action, name)                            \
     HPX_UTIL_REGISTER_UNIQUE_FUNCTION(                                        \
-        void (hpx::naming::id_type, hpx::agas::response)                      \
+        void (hpx::naming::id_type, hpx::naming::id_type)                     \
       , (hpx::util::functional::detail::apply_continuation_impl<              \
             hpx::util::detail::bound_action<                                  \
                 action                                                        \
@@ -69,6 +65,5 @@ namespace hpx { namespace detail
       , name                                                                  \
     );                                                                        \
 /**/
-#endif
 
 #endif

hpx/util/functional/colocated_helpers.hpp
@@ -16,7 +16,6 @@
 #include <hpx/throw_exception.hpp>
 #include <hpx/util/decay.hpp>
 #include <hpx/util/result_of.hpp>
-#include <hpx/util/tuple.hpp>
 
 #include <memory>
 #include <utility>
@@ -31,15 +30,9 @@ namespace hpx { namespace util { namespace functional
     {
         extract_locality() {}
 
-        typedef
-            hpx::util::tuple<naming::gid_type, agas::gva, naming::gid_type>
-            resolved_type;
-
-        naming::id_type operator()(resolved_type const& rep,
+        naming::id_type operator()(naming::id_type const& locality_id,
             naming::id_type const& id) const
         {
-            naming::id_type locality_id(
-                hpx::util::get<2>(rep), naming::id_type::unmanaged);
             if(locality_id == naming::invalid_id)
             {
                 HPX_THROW_EXCEPTION(hpx::no_success,