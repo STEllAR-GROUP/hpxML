zkhatami88/hpx

Allow one to pass rdma key into serialization chunk create

hpx/runtime/serialization/serialization_chunk.hpp
@@ -53,10 +53,10 @@ namespace hpx { namespace serialization
         return retval;
     }
 
-    inline serialization_chunk create_pointer_chunk(void const* pos, std::size_t size)
+    inline serialization_chunk create_pointer_chunk(void const* pos, std::size_t size, boost::uint32_t rkey=0)
     {
         serialization_chunk retval = {
-            { 0 }, size, static_cast<boost::uint8_t>(chunk_type_pointer), 0
+            { 0 }, size, static_cast<boost::uint8_t>(chunk_type_pointer), rkey
         };
         retval.data_.cpos_ = pos;
         return retval;

STEllAR-GROUP/hpxML

hpx/runtime/serialization/serialization_chunk.hpp
@@ -56,10 +56,10 @@ namespace hpx { namespace serialization
         return retval;
     }
 
-    inline serialization_chunk create_pointer_chunk(void const* pos, std::size_t size)
+    inline serialization_chunk create_pointer_chunk(void const* pos, std::size_t size, std::uint32_t rkey=0)
     {
         serialization_chunk retval = {
-            { 0 }, size, static_cast<std::uint8_t>(chunk_type_pointer), 0
+            { 0 }, size, static_cast<std::uint8_t>(chunk_type_pointer), rkey
         };
         retval.data_.cpos_ = pos;
         return retval;