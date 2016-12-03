zkhatami88/hpx

Fix warning about uninitialized var, remove debug code
Remove serialization::disable_xxx optimization code used during debugging

hpx/runtime/serialization/serialization_chunk.hpp
@@ -47,7 +47,7 @@ namespace hpx { namespace serialization
     inline serialization_chunk create_index_chunk(std::size_t index, std::size_t size)
     {
         serialization_chunk retval = {
-            { 0 }, size, static_cast<boost::uint8_t>(chunk_type_index)
+            { 0 }, size, static_cast<boost::uint8_t>(chunk_type_index), 0
         };
         retval.data_.index_ = index;
         return retval;

STEllAR-GROUP/hpxML

hpx/runtime/serialization/serialization_chunk.hpp
@@ -50,7 +50,7 @@ namespace hpx { namespace serialization
     inline serialization_chunk create_index_chunk(std::size_t index, std::size_t size)
     {
         serialization_chunk retval = {
-            { 0 }, size, static_cast<std::uint8_t>(chunk_type_index)
+            { 0 }, size, static_cast<std::uint8_t>(chunk_type_index), 0
         };
         retval.data_.index_ = index;
         return retval;