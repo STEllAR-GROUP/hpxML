zkhatami88/hpx

Implement zero-copy chunk get in verbs parcelport using rdma read from destination
Some changes to header and decode parcels to improve performance of the verbs
parcelport. Serialization chunks can store an rdma key and the decode parcels
functin can be entered with the chunk info already supplied.

hpx/runtime/serialization/serialization_chunk.hpp
@@ -36,9 +36,10 @@ namespace hpx { namespace serialization {
 
     struct serialization_chunk
     {
-        chunk_data data_;       // index or pointer
-        std::size_t size_;      // size of the serialization_chunk starting at index_/pos_
-        boost::uint8_t type_;   // chunk_type
+        chunk_data      data_;  // index or pointer
+        std::size_t     size_;  // size of the serialization_chunk starting at index_/pos_
+        boost::uint8_t  type_;  // chunk_type
+        boost::uint32_t rkey_;  // optional RDMA remote key for parcelport put/get operations
     };
 
     ///////////////////////////////////////////////////////////////////////

STEllAR-GROUP/hpxML

hpx/runtime/serialization/serialization_chunk.hpp
@@ -40,10 +40,10 @@ namespace hpx { namespace serialization
 
     struct serialization_chunk
     {
-        chunk_data data_;       // index or pointer
-        std::size_t size_;
-        // size of the serialization_chunk starting at index_/pos_
-        std::uint8_t type_;   // chunk_type
+        chunk_data    data_; // index or pointer
+        std::size_t   size_; // size of the serialization_chunk starting index_/pos_
+        std::uint8_t  type_; // chunk_type
+        std::uint32_t rkey_; // optional RDMA remote key for parcelport put/get operations
     };
 
     ///////////////////////////////////////////////////////////////////////