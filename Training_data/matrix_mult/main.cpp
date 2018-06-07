639543b2a1833010b2898fea7089da684f682d5a
Gabriel Laberge <gabriel.laberge@polymtl.ca>
2018-06-07 16:50:52 -0500

    Debug

--- Training_data/matrix_mult/main.cpp
+++ Training_data/matrix_mult/main.cpp
@@ -17,35 +17,13 @@
 #include <typeinfo>
 #include <iterator>
 #include <hpx/parallel/executors/dynamic_chunk_size.hpp>
-#include "lambda/1_loop_lambda.h"
-#include "lambda/2_loop_lambda.h"
-
-
-
-double random_double(double min,double max){
-    return (min+1)+(((double) rand())/(double) RAND_MAX)*(max-(min+1));
-}
-
-template<typename T>
-void vector_generator(std::vector<T> &A,int size,char type){
-    if(type=='f'){
-        for(std::size_t r = 0; r < size; r++) {
-	    A.push_back(random_double(1,100));
-	}
-    }
-    else if(type=='i'){
-        for(std::size_t r = 0; r < size; r++) {
-	    A.push_back((r+10)*(rand() % 10 + 1));
-	}
-    }
-}
-	
-        
+#include "algorithms/1_loop_level.h"
+#include<string>
 
 int hpx_main(int argc, char* argv[])
 {
     // Initialization
-    int iterations=strtol(argv[3],NULL,10);
+    int iterations=strtol(argv[2],NULL,10);
     std::vector<double> chunk_candidates(7);
     chunk_candidates[0]=0.001;chunk_candidates[1]=0.005;chunk_candidates[2]=0.01;chunk_candidates[3]=0.05;chunk_candidates[4]=0.1;
     chunk_candidates[5]=0.2;chunk_candidates[6]=0.5;
@@ -53,117 +31,27 @@ int hpx_main(int argc, char* argv[])
     srand(time(NULL));
     std::ofstream file("./files/train_data_matrix_debug.txt",std::ios::app);
     if(file){
-        if(argv[1][6]=='1' && argv[1][8]=='1'){
-	    if(argv[2][0]=='i'){
-	        std::vector<int> A;
-		std::vector<int> B;
-		std::vector<int> C;
-		vector_generator(A,iterations,argv[2][0]);
-		vector_generator(B,iterations,argv[2][0]);
-		vector_generator(C,iterations,argv[2][0]);
-                lambda1_1(A,B,C,iterations,chunk_candidates,file);
-		}
-	    else if(argv[2][0]=='f'){
-	        std::vector<double> A;
-		std::vector<double> B;
-		std::vector<double> C;
-		vector_generator(A,iterations,argv[2][0]);
-		vector_generator(B,iterations,argv[2][0]);
-		vector_generator(C,iterations,argv[2][0]);
-		lambda1_1(A,B,C,iterations,chunk_candidates,file);    
-	    }
+        if(std::strncmp(argv[1],"Rand_Pond_Sum",13)==0){     
+		Rand_Pond_Sum(iterations,chunk_candidates,file); 
+		std::cout<<"Rans_Sum"<<std::endl;
         }
        
-        else if(argv[1][6]=='1' && argv[1][8]=='2'){
-	    if(argv[2][0]=='i'){
-	        std::vector<int> A;
-		std::vector<int> B;
-		vector_generator(A,iterations,argv[2][0]);
-		vector_generator(B,iterations,argv[2][0]);
-                lambda1_2(A,B,iterations,chunk_candidates,file);
-		}
-	    else if(argv[2][0]=='f'){
-	        std::vector<double> A;
-		std::vector<double> B;
-		vector_generator(A,iterations,argv[2][0]);
-		vector_generator(B,iterations,argv[2][0]);
-		lambda1_2(A,B,iterations,chunk_candidates,file);    
-	    }
-        }
+        else if(std::strncmp(argv[1],"Swap",4)==0){
+		Swap(iterations,chunk_candidates,file);    
+		std::cout<<"Swap"<<std::endl;
+	}
+        else if(std::strncmp(argv[1],"Finite_Diff_Step",16)==0){
+		Finite_Diff_Step(iterations,chunk_candidates,file);    
+		std::cout<<"Finite_diff"<<std::endl;
+	}
+        else if(std::strncmp(argv[1],"Stream",6)==0){
+		Stream(iterations,chunk_candidates,file);    
+		std::cout<<"Stream"<<std::endl;
+	}
 
-        else if(argv[1][6]=='2' && argv[1][8]=='1'){
-	    if(argv[2][0]=='i'){
-	        std::vector<int> A;
-		std::vector<int> B;
-		std::vector<int> C;
-		vector_generator(A,iterations*iterations,argv[2][0]);
-		vector_generator(B,iterations*iterations,argv[2][0]);
-		vector_generator(C,iterations*iterations,argv[2][0]);
-                lambda2_1(A,B,C,iterations,chunk_candidates,file);
-		}
-	    else if(argv[2][0]=='f'){
-	        std::vector<double> A;
-		std::vector<double> B;
-		std::vector<double> C;
-		vector_generator(A,iterations*iterations,argv[2][0]);
-		vector_generator(B,iterations*iterations,argv[2][0]);
-		vector_generator(C,iterations*iterations,argv[2][0]);
-		lambda2_1(A,B,C,iterations,chunk_candidates,file);    
-	    }
-        } 
-	else if(argv[1][6]=='2' && argv[1][8]=='2'){
-	    if(argv[2][0]=='i'){
-	        std::vector<int> A;
-		std::vector<int> B;
-		vector_generator(A,iterations*iterations,argv[2][0]);
-		vector_generator(B,iterations*iterations,argv[2][0]);
-                lambda2_2(A,B,iterations,chunk_candidates,file);
-		}
-	    else if(argv[2][0]=='f'){
-	        std::vector<double> A;
-		std::vector<double> B;
-		vector_generator(A,iterations*iterations,argv[2][0]);
-		vector_generator(B,iterations*iterations,argv[2][0]);
-		lambda2_2(A,B,iterations,chunk_candidates,file);    
-	    }
-        }
-        else if(argv[1][6]=='2' && argv[1][8]=='3'){
-	    if(argv[2][0]=='i'){
-	        std::vector<int> A;
-		std::vector<int> B;
-		vector_generator(A,iterations*iterations,argv[2][0]);
-		vector_generator(B,iterations*iterations,argv[2][0]);
-                lambda2_3(A,B,iterations,chunk_candidates,file);
-		}
-	    else if(argv[2][0]=='f'){
-	        std::vector<double> A;
-		std::vector<double> B;
-		vector_generator(A,iterations*iterations,argv[2][0]);
-		vector_generator(B,iterations*iterations,argv[2][0]);
-		lambda2_3(A,B,iterations,chunk_candidates,file);    
-	    }
-        }
-        else if(argv[1][6]=='2' && argv[1][8]=='4'){
-	    if(argv[2][0]=='i'){
-	        std::vector<int> A;
-		std::vector<int> B;
-		std::vector<int> C;
-		vector_generator(A,iterations*iterations,argv[2][0]);
-		vector_generator(B,iterations*iterations,argv[2][0]);
-		vector_generator(C,iterations*iterations,argv[2][0]);
-                lambda2_4(A,B,C,iterations,chunk_candidates,file);
-		}
-	    else if(argv[2][0]=='f'){
-	        std::vector<double> A;
-		std::vector<double> B;
-		std::vector<double> C;
-		vector_generator(A,iterations*iterations,argv[2][0]);
-		vector_generator(B,iterations*iterations,argv[2][0]);
-		vector_generator(C,iterations*iterations,argv[2][0]);
-		lambda2_4(A,B,C,iterations,chunk_candidates,file);    
-	    }
-        }
-
+    }
+    else{
+        std::cout<<"The file could not open"<<std::endl;
     }
     return hpx::finalize();
 }
