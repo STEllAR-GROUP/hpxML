2b47b4599c43f74f3f578defeee233e4ed1f2afa
Gabriel Laberge <gabriel.laberge@polymtl.ca>
2018-07-30 11:08:44 -0500

    Add corrections to chunk)size_determiation

--- hpxmlOld/hpx/parallel/chunk_size_determination.hpp
+++ hpxmlOld/hpx/parallel/chunk_size_determination.hpp
@@ -38,9 +38,22 @@ namespace hpx { namespace parallel {
     	}
 
         //candidates
-        double candidates[] = {0.001, 0.01, 0.1, 0.5};
+        double candidates[] = {0.5,1/8,1/32,1/128,1/512};
+        //manually modify obvious errors
+	
 
+        // Chunk_Size>1/threads is an error
+	while(1/candiates[determined_class]<features[0]){
+	    determined_class+=1;
+	}
+        
     	double chunk_size = candidates[determined_class] * features[4];
+
+        //CS*size<1 is an error
+	if(candidates[determined_class]*features[4]<1){
+	    chunk_size=1;
+	}
+
 
     	return hpx::parallel::dynamic_chunk_size(chunk_size);
 	}
