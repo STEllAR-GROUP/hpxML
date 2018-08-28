3bf3162227b48fc8a0556feb845e03f126a5ca87
Gabriel Laberge <gabriel.laberge@polymtl.ca>
2018-08-28 13:31:56 -0500

    Modify main.cpp

--- Time_measurement/main.cpp
+++ Time_measurement/main.cpp
@@ -1,65 +1,41 @@
-// Copyright (c) 2018 Gabriel Laberge
+//  Copyright (c) 2018 Gabriel Laberge
+//
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-#include <stdlib.h>
-#include<ctime>
+#include <cstdlib>
+#include <ctime>
 #include <vector>
-#include<fstream>
-#include <hpx/hpx_init.hpp>
-#include <hpx/parallel/algorithms/for_each.hpp>
-#include <hpx/util/high_resolution_timer.hpp>
-#include <boost/range/irange.hpp>
-#include <vector>
+#include <fstream>
+#include <string>
 #include <initializer_list>
 #include <algorithm>
 #include <typeinfo>
 #include <iterator>
+#include <map>
+#include <hpx/hpx_init.hpp>
+#include <hpx/parallel/algorithms/for_each.hpp>
+#include <hpx/util/high_resolution_timer.hpp>
 #include <hpx/parallel/executors/dynamic_chunk_size.hpp>
-#include <hpx/runtime/get_os_thread_count.hpp>
-#include "../Training_data/algorithms/dynamic_and_times/1_loop_level.h"
-#include "../Training_data/algorithms/dynamic_and_times/2_loop_level.h"
-#include "../Training_data/algorithms/dynamic_and_times/3_loop_level.h"
-#include "../Training_data/algorithms/dynamic_and_times/4_loop_level.h"
-#include<string>
+#include "../Training_data/algorithms/dynamic_and_times/all_functions.hpp"
 
 int hpx_main(int argc, char* argv[])
 {
     // Initialization 
-    int iterations=strtol(argv[2],NULL,10);
-    std::vector<double> chunk_candidate(1);
-    chunk_candidate[0]=strtof(argv[3],NULL);
+    //
+    Map_pointers OPTIONS;
+    int iterations=strtol(argv[2], NULL, 10);
+    std::vector<double> chunk_candidates(1);
 
-    if(std::strncmp(argv[1],"Nothing",7)==0){
-        Nothing(iterations,chunk_candidate,false);
+    chunk_candidates[0]=strtof(argv[3],NULL);
+    Fill_map(OPTIONS);
+   
+    auto function=OPTIONS.find(argv[1]);
+    if(function != OPTIONS.end()) {
+        (*function).second(iterations, chunk_candidates, false);
     }
-    else if(std::strncmp(argv[1],"Swap",4)==0){
-	Swap(iterations,chunk_candidate,false);    
-    }
-    else if(std::strncmp(argv[1],"Stream",6)==0){
-	Stream(iterations,chunk_candidate,false);   
-    }
-    else if(std::strncmp(argv[1],"Matrix_Vector_Mult",18)==0){
-	Matrix_Vector_Mult(iterations,chunk_candidate,false);    
-    }
-    else if(std::strncmp(argv[1],"Diadic_Prod",11)==0){
-	Diadic_Prod(iterations,chunk_candidate,false);   
-    }
-    else if(std::strncmp(argv[1],"Cosine",6)==0){
-	Cosine(iterations,chunk_candidate,false);   
-    }
-    else if(std::strncmp(argv[1],"Matrix_Matrix_Mult",20)==0){
-        Matrix_Matrix_Mult(iterations,chunk_candidate,false);
-    }
-    else if(std::strncmp(argv[1],"Tensor_generator",16)==0){
-        Tensor_generator(iterations,chunk_candidate,false);
-    }
-    else if(std::strncmp(argv[1],"Max",3)==0){
-        Max(iterations,chunk_candidate,false);
-    }
-    else if(std::strncmp(argv[1],"Stencil",7)==0){
-       Stencil(iterations,chunk_candidate,false);
-    }
-    else{ std::cout<<"Function not found"<<std::endl;}
+    else{ std::cout<< "Function not found" <<std::endl;}
+
+
     return hpx::finalize();
 }
 
